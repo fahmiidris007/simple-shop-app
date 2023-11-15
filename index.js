aab845c8cf9c0d8eaf0589d1a835cee9313f1a1f
fahmiidris <fahmiidris@apps.ipb.ac.id>
2023-11-15 11:41:51 +0700

    add feature edit product and update feature add product

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const app = express();
 
@@ -19,6 +20,8 @@ mongoose
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
   res.send('Home Page');
@@ -33,10 +36,31 @@ app.get('/products/new', (req, res) => {
   res.render('products/new');
 });
 
+app.post('/products', async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
